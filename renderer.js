974c6f72dad63e1088461328cd190d5729580f6d
Daniel <daniel.elliott.lewis@gmail.com>
2020-09-09 00:01:39 +1000

    demo for webview & blink integration problem

--- renderer.js
+++ renderer.js
@@ -4,3 +4,11 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const webview = document.getElementsByTagName("webview")[0]
+
+console.log("got webview: ", webview)
+
+webview.addEventListener("dom-ready", () => {
+  webview.openDevTools()
+})
